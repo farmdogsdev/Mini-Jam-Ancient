c88373cb321c988d9342e2a7c242cf7b1cc39f7d
farmdogs <122422883+farmdogsdev@users.noreply.github.com>
2024-01-05 20:30:46 -0600

    basic slide movement

--- Scenes/character.gd
+++ Scenes/character.gd
@@ -1,30 +1,29 @@
 # adapted from https://kidscancode.org/godot_recipes/4.x/2d/grid_movement/index.html
-extends Area2D
+extends CharacterBody2D
 
 var tile_size = 112
 var screen_size
+var speed = 400
 var moving = false
-
-var inputs = {"move_right": Vector2.RIGHT,
-			"move_left": Vector2.LEFT,
-			"move_up": Vector2.UP,
-			"move_down": Vector2.DOWN}
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	position = Vector2(tile_size, tile_size)
-
-func _unhandled_input(event):
-	for dir in inputs.keys():
-		while event.is_action_pressed(dir):
-			move(dir)
 			
-func move(dir):
-	if moving:
-		return
-	position += inputs[dir] * tile_size/2
-	position = position.clamp(Vector2.ZERO, screen_size)
 
-func _process(delta):
-	pass
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
 	
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	position = position.clamp(Vector2.ZERO, screen_size)
